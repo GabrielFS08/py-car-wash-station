b5570285f3f192ea5cf09747044f5c1db6c3db73
Gabriel <gferrazsiqueira05@gmail.com>
2026-03-26 07:31:47 -0300

    Resolução

--- app/main.py
+++ app/main.py
@@ -8,30 +8,30 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int,
+    def __init__(self, distance_from_city_center: float,
                  clean_power: int,
-                 average_rating: int,
+                 average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car: str) -> float:
-        diff: object = self.clean_power - car.clean_mark
+    def calculate_washing_price(self, car: Car) -> float:
+        diff = self.clean_power - car.clean_mark
         if diff <= 0:
             return 0.0
         price = (car.comfort_class
                  * diff * self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car: str) -> bool:
+    def wash_single_car(self, car: Car) -> bool:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
             return True
         return False
 
-    def serve_cars(self, cars_list: str) -> float:
+    def serve_cars(self, cars_list: list[Car]) -> float:
         total = 0.0
         for car in cars_list:
             price = self.calculate_washing_price(car)
